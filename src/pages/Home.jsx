3ff1f67480fab44d6d4f5bcb507c5fdfd117a051
augustospr <augusto_spr@hotmail.com>
2023-03-23 01:54:23 -0300

    setting the fetch api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,23 @@ import { useState } from "react";
 
 export default function Home() {
 
+  const url = "https://pokeapi.co/api/v2/pokemon?limit=50&offset=0";
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch(url);
+      const json = await data.json();
+      return json;
+    }
+    const result = fetchData()
+      .catch(console.error);;
+    setData(result);
+  }, [])
+
+  console.log(data);
+
   return (
     <>
       <Container sx={{ paddingBottom: 5 }}>
