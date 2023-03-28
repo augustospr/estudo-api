9fee044fc130bb651acec7f4cbd9928b0d592d76
augustospr <augusto_spr@hotmail.com>
2023-03-28 18:18:23 -0300

    att

--- src/components/cardPokemon/CardPokemon.jsx
+++ src/components/cardPokemon/CardPokemon.jsx
@@ -8,7 +8,7 @@ import Typography from '@mui/material/Typography';
 export default function CardPokemon({ pokemon, types }) {
 
 	const typeHandler = () => {
-		if (types[1]) {
+		if (types[1].type.name) {
 			return types[0].type.name + " | " + types[1].type.name;
 		}
 		else {
